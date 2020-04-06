b5c96c31404a3180664416b536300913000fc800
Daniel Garcia <demonpogp@gmail.com>
2020-04-06 11:23:27 +0200

    haciendo mas pruebas

--- tbot_contrib/register/pruebas.py
+++ tbot_contrib/register/pruebas.py
@@ -1,3 +1,19 @@
+import typing
+
 dic: dict = {}
 
-print(dic["hola"])
+print()
+
+
+def get_register(dic: dict, name: str) -> typing.Any:
+    if name in dic:
+        return dic[name]
+    raise Exception(f"There is not a register with name: {name}")
+
+
+def get_all_registers(dic: dict) -> typing.List[str]:
+    return list(dic.values())
+
+
+# get_register(dic,"aloha")
+print(get_all_registers(dic))
--- tbot_contrib/register/register.py
+++ tbot_contrib/register/register.py
@@ -16,16 +16,21 @@ class RegisterMap:
                 self.registers_dict[register.name] = register
 
     def add_register(self, register: Register) -> None:
+        if register.name in self.registers_dict:
+            raise Exception(
+                f"There is already a register with the name: {register.name}"
+            )
         self.registers_dict[register.name] = register
 
     def remove_register(self, name: str) -> Register:
-        return self.registers_dict.pop(name)
+        if name in self.registers_dict:
+            return self.registers_dict.pop(name)
+        raise Exception(f"There is not a register with the name: {name}")
 
     def get_register(self, name: str) -> Register:
         if name in self.registers_dict:
             return self.registers_dict[name]
-
-        return self.registers_dict[name]
+        raise Exception(f"There is not a register with the name: {name}")
 
     def get_all_registers(self) -> typing.List[Register]:
         return list(self.registers_dict.values())
