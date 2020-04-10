41d74d76170e79436a0e77c2baf54a3db408898b
Daniel Garcia <demonpogp@gmail.com>
2020-04-10 15:04:25 +0200

    se modifico la clase register que esta en el archivo testcase para que trabaje con diferentes tipos de maquinas

--- tbot_contrib/boardConfig/testcase.py
+++ tbot_contrib/boardConfig/testcase.py
@@ -1,37 +1,30 @@
 import tbot
 import typing
 from tbot.machine import Machine, linux, board
-
+import contextlib
 
 
 class Register:
-    def __init__(self, host: Machine, name: str, address: int, width: int):
-        self._host =host
+    def __init__(self, name: str, address: int, width: int):
         self.name = name
         self.address = address
         assert width in [8, 16, 32, 64], f"Unsupported register width: {width!r}"
         self.width = width
+       
+    def getValue(self, host: Machine) -> None:
         if isinstance(host, board.UBootShell):
-            self._machine = "uboot"
+            pass
         elif isinstance(host, linux.LinuxShell):
-            self._machine = "linux"
+            pass
         else:
             raise Exception(
                 f"You are not in allowed context (linux machine or uboot machine)"
             )
 
-    def getValue(self) -> None:
-        if self._machine == "uboot":
-            pass
-        elif self._machine == "linux":
-            pass
-
-
-
-@tbot.testcase
 def testcase_prueba() -> None:
     with tbot.acquire_lab() as lb:
         lb.exec0("whoami")
         with tbot.acquire_board(lb) as b:
+            register1= Register("prueba",1231,16)
             with tbot.acquire_linux(b) as lx:
-                register1= Register(lx,"prueba",1231,16)
+                register1.getValue(lx)
